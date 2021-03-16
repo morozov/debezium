2e5783928788ed062010c4b7f35c2be0bad45e33
Jacob Gminder <jacob.gminder@sugarcrm.com>
2021-03-16 10:00:03 -0400

    DBZ-2975: Move the poll interval logic to the ChangeEventSourceCoordinator.

--- debezium-connector-sqlserver/src/main/java/io/debezium/connector/sqlserver/SqlServerConnectorTask.java
+++ debezium-connector-sqlserver/src/main/java/io/debezium/connector/sqlserver/SqlServerConnectorTask.java
@@ -127,7 +127,8 @@ public class SqlServerConnectorTask extends BaseSourceTask<SqlServerTaskPartitio
                 new SqlServerChangeEventSourceFactory(connectorConfig, dataConnection, metadataConnection, errorHandler, dispatcher, clock, schema),
                 new DefaultChangeEventSourceMetricsFactory(),
                 dispatcher,
-                schema);
+                schema,
+                clock);
 
         coordinator.start(taskContext, this.queue, metadataProvider);
 
